80024c7e52e55d04f766c7d7aa8ffa76da6f4dd5
muddi <muddi@10.0.0.174>
2023-08-18 21:11:22 -0400

    Homework for class 27.

---
+++ src/Student_Maria/Task1ForLoop.java
@@ -0,0 +1,72 @@
+package Student_Maria;
+
+public class Task1ForLoop {
+
+	public static void main(String[] args) {
+		
+		
+	  //  For  loop //	
+		
+
+System.out.println("***************** for loop task 1 *******************************");
+     
+	for(int i=2; i<=-10; i++){
+	System.out.println("print i = "+ i);	
+		
+	}	
+
+System.out.println("***************** for loop task 2 *******************************");	
+	for(int a=3; a<=20; a++)	 {
+	System.out.println(a);	
+	}	
+		
+		
+
+System.out.println("***************** for loop task 3 *******************************");		
+	for(int b=-355; b<=-255; b++)  {	
+	System.out.println("print the value of b"+ b);	
+		
+	}
+		
+
+System.out.println("***************** for loop task 4 *******************************");		
+	for(int c=-956; c>=-960; c--) {	
+	System.out.println("print the value of c"+ c);	
+		
+	}	
+
+System.out.println("***************** for loop task 5 *******************************");		
+		
+	for(int m=-10; m<=10; m++) {
+	System.out.println("print d " + m);	
+	}	
+		
+		
+
+System.out.println("*****************break tesk 1 with for loop *******************************");
+	for(int mim=-5; mim<5;mim++) {
+	if(mim==3) {
+	break;
+	}
+	System.out.println(mim);
+	}
+		
+	
+
+System.out.println("*****************break tesk 2 with for loop *******************************");
+	
+	for(int ramin=-500;ramin<=-200;ramin++) {
+	if (ramin==-278) {
+	break;
+	}System.out.println(ramin);
+	}
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
---
+++ src/Student_Maria/Task1WhileLoop.java
@@ -0,0 +1,69 @@
+package Student_Maria;
+
+public class Task1WhileLoop {
+
+	public static void main(String[] args) {
+		
+		// while loop  //
+		
+	int i = 2;
+	while(i>=-10);{
+System.out.println("print the value of i"+ i);
+i--;
+	}	
+		
+int i1=3;
+while(i1<=20);{
+System.out.println("print the i1"+ i1);
+i1++;
+}		
+	
+
+int i2= -355;
+while(i2<=-255); {
+System.out.println("print thr i2"+ i2);
+i2++;
+}
+
+
+int i3=-956;
+while(i3>=-960); {
+System.out.println("print the velue of i3"+ i3);
+i3++;
+}
+		
+	
+
+int i4=-10;
+while(i4<=10); {
+System.out.println("print the velue of i4"+ i4);
+i4++;
+}
+
+
+int i5= -5;
+while(i5<=5); {
+System.out.println("print i5"+ i5);
+
+if(i5==3) {
+System.out.println("we got the value");
+  i5++;}
+
+
+}
+
+
+int i6=-500;
+while(i6<=-200) {
+System.out.println("print i6"+ i6);
+if(i6==-278) {
+System.out.println("the value of");
+break;
+}i6++;
+
+}
+
+		
+	}
+
+}
