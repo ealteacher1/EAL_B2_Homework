ccf1f5a26ebb00aab94be6f4d120da90d9cf696e
mojum <mojum@LAPTOP-C50CP82R.mynetworksettings.com>
2023-09-02 00:12:56 -0400

    Hi, This is Lubna

---
+++ src/Lubna/Class_33_Task_1/Java_Inheritance_Class33_Task_2/S6_T2_Q1_C1.java
@@ -0,0 +1,24 @@
+package Java_Inheritance_Class33_Task_2;
+
+public class S6_T2_Q1_C1 {
+	
+     static  int a=50;
+	public static void main(String[] args) {
+	
+
+	}
+	
+	public void test_c1_1() {
+  System.out.println("test_c1_1");
+  }
+	
+    public void test_c1_2() {
+	   System.out.println("test_c1_2");
+	   
+
+   }
+    
+    public void test_c1_3() {
+    	System.out.println("test_c1_3"+ a);
+    }
+}
---
+++ src/Lubna/Class_33_Task_1/Java_Inheritance_Class33_Task_2/S6_T2_Q1_C2.java
@@ -0,0 +1,31 @@
+package Java_Inheritance_Class33_Task_2;
+
+public class S6_T2_Q1_C2 extends S6_T2_Q1_C1{
+	
+	
+  static int a= 50;
+  
+  
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void test_c2_1() {
+		System.out.println("test_c2_1");
+	}
+	
+	public void test_C2_2() {
+		System.out.println("test_c2_2");
+	}
+  public void test_c2_3() {
+	  System.out.println("test_c2_3");
+  }
+  public void test_c1_1() {
+	  System.out.println("test_c1_1");
+	  }
+		
+	    public void test_c1_2() {
+		   System.out.println("test_c1_2");
+	    }	   	
+}
